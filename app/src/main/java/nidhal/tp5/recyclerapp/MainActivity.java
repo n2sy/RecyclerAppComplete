8e59e11966d8699117e410f379b454c49434705d
nidhal <nidhal.jelassi@fsegt.utm.tn>
2020-11-15 17:26:32 +0100

    first commit

---
+++ app/src/main/java/nidhal/tp5/recyclerapp/MainActivity.java
@@ -0,0 +1,108 @@
+package nidhal.tp5.recyclerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    //on aurait pu utiliser une autre structure de données comme  :
+    //private final List<String> wordsList = new ArrayList<>();
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Ajouter des éléments au RecyclerView.
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Element " + i);
+        }
+
+        // Créer une variable de type reycylerView
+        mRecyclerView = findViewById(R.id.recyclerview);
+
+        // Créer l'Adapter et lui passer la liste dont il va afficher les éléments
+        mAdapter = new WordListAdapter(this, mWordList);
+
+        // Connecter l'Adapter à notre RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Passer au RecyclerView le LayoutManager désiré
+        LinearLayoutManager l = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(l);
+
+        //Ajouter un ActionBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        //Ajouter un Actionbutton
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                // Ajouter un nouveau mot à notre liste mWordList
+                mWordList.addLast("+ New Element " + wordListSize);
+                // Notifier l'Adapter que la liste a été modifiée.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                // Scroller en bas.
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        //L'Inflater va ajouter les élements du menu au Menu de l'ActionBar
+        getMenuInflater().inflate(R.menu.menu_main, m);
+
+        //Si il y a eu un problème avec l'ajout du menu, ca déclenchera une exception
+        //Sinon, on renvoie true
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Récupérer l'Id de l'item selectionné
+        int id = item.getItemId();
+
+        // Tester l'id de l'élément selectionné
+        if (id == R.id.action_settings1) {
+            Toast.makeText(getApplicationContext(), R.string.action_settings1, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(id == R.id.action_settings2) {
+            Toast.makeText(getApplicationContext(), R.string.action_settings2, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(id == R.id.action_settings3) {
+            Toast.makeText(getApplicationContext(), R.string.action_settings3, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else {
+            Toast.makeText(getApplicationContext(), R.string.action_settings4, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+    }
+}
+
---
+++ app/src/main/java/nidhal/tp5/recyclerapp/WordListAdapter.java
@@ -0,0 +1,78 @@
+package nidhal.tp5.recyclerapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    //Préparer la structure qui contiendra les éléments de notre liste
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int mPosition = getLayoutPosition();
+
+            String element = mWordList.get(mPosition);
+            String newElement = "Clicked! "+ element;
+            mWordList.set(mPosition, newElement);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    //Appelée au moment de la création du ViewHolder qui affichera
+    //les éléments chargés à partir de l'Adapter
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        // Inflate an item view.
+        View mItemView = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    //Elle est appelé à chaque fois qu'une vue doit être chargé.
+    //On récupére alors la position du nouvel élément à afficher
+    //et on le charge dans le TextView
+    @Override
+    public void onBindViewHolder(WordListAdapter.WordViewHolder holder,
+                                 int position) {
+        // Retrieve the data for that position.
+        String mCurrent = mWordList.get(position);
+        // Add the data to the view holder.
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    //Retourne le nombre d'éléments de notre liste
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
